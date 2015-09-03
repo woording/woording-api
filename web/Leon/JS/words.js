7e8083268ac7e7e942a33a72a76523a64e8ac431
Leon <Leongrasmeijer@gmail.com>
2015-09-03 20:47:01 +0200

    Removed document specific items from words.js

--- web/Leon/JS/words.js
+++ web/Leon/JS/words.js
@@ -1,10 +1,3 @@
-var currentWord = document.getElementById("currentWord");
-var listTitle = document.getElementById("listTitle");
-var rightOrWrong = document.getElementById("rightOrWrong");
-var nextButton = document.getElementById("checkWord");
-var answerInput = document.getElementById("answerInput");
-
-
 var usedWords = [];
 
 nextButton.onclick = function(){
