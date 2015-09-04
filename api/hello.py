cf7b66e3324778896ff8918178d73ed158b2f3a5
cor <cor@pruijs.nl>
2015-09-04 09:46:32 +0200

    Add basic REST implementation

--- api/hello.py
+++ api/hello.py
@@ -1,21 +1,35 @@
 from flask import Flask
+from flask_restful import Resource, Api
+
+
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def hello_world():
-	return 'Hello World!'
+class User(Resource):
+	def get(self, username):
+		return {'name': username, 'lists': ['lijst duits', 'lijst 2']}
 
-@app.route('/<username>')
-def show_user_profile(username):
-	return 'Hello ' + username
+class List(Resource):
+	def get(self, username, listname):
+		return {'name': listname, 'languages': {'language_1' : 'Nederlands', 'language_2' : 'Engels'}, 'words': [{'language_1' : 'auto', 'language_2' : 'car'}, {'language_1' : 'boom', 'language_2' : 'tree'}]}
 
-@app.route('/<username>/<list>')
-def show_user_list(username, list):
-	return 'Show list ' + username + '/' + list 
 
-@app.route('/about')
-def show_about():
-	return 'Insert about page here'
+
+api.add_resource(User, '/<username>')
+api.add_resource(List, '/<username>/<listname>')
+
+
+# @app.route('/<username>')
+# def show_user_profile(username):
+# 	return 'Hello ' + username
+
+# @app.route('/<username>/<list>')
+# def show_user_list(username, list):
+# 	return 'Show list ' + username + '/' + list 
+
+# @app.route('/about')
+# def show_about():
+# 	return 'Insert about page here'
 
 if __name__ == '__main__':
-	app.run('0.0.0.0')
+	app.run('0.0.0.0', debug=True)
