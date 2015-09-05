ea4e4e6b946f709968174e07a80356043e6e6061
Leon <Leongrasmeijer@gmail.com>
2015-09-05 22:57:27 +0200

    Better data fetching for angular

--- web/Leon/JS/controllers/MainController.js
+++ web/Leon/JS/controllers/MainController.js
@@ -1,6 +1,6 @@
 app.controller('MainController', function($scope, $http) {
 	$scope.title = 'Wording';
-	$http.get('http://127.0.0.1:5000/leon').
+	$http.get('http://127.0.0.1:5000/' + window.location.hash.substring(1)).
 		success(function(data, status, headers, config) {
 			$scope.jsonData = data;
 		}).
