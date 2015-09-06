a627db4577a39e2a4936948ffbb43693bf1e7315
Leon <Leongrasmeijer@gmail.com>
2015-09-06 18:18:18 +0200

    Updated some web stuff

--- web/Leon/JS/controllers/MainController.js
+++ web/Leon/JS/controllers/MainController.js
@@ -1,10 +1,13 @@
 app.controller('MainController', function($scope, $http) {
 	$scope.title = 'Wording';
-	$http.get('http://127.0.0.1:5000/' + window.location.hash.substring(1)).
-		success(function(data, status, headers, config) {
-			$scope.jsonData = data;
-		}).
-		error(function(data, status, headers, config) {
-			console.log("error");
-		});
+	$scope.loadData = function(){
+		$http.get('http://127.0.0.1:5000/' + window.location.hash.substring(1)).
+			success(function(data, status, headers, config) {
+				$scope.jsonData = data;
+			}).
+			error(function(data, status, headers, config) {
+				console.log("error");
+			}); 
+	}	
+	$scope.loadData();
 });
