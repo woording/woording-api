78cf2118445ff2378aeaef68b1682334b640bd67
PhiliPdB <phlpdbrn@gmail.com>
2015-09-07 17:34:24 +0200

    Add working email verification to register function

--- api/api.py
+++ api/api.py
@@ -1,15 +1,30 @@
-from flask import Flask, request
+from flask import Flask, request, abort, url_for, render_template
 from flask import g
 from flask_restful import Resource, Api
 from flask.ext.httpauth import HTTPBasicAuth
+from flask_mail import Mail
 from passlib.hash import sha512_crypt
 import json
 from corsDecorator import crossdomain
 from database import DatabaseManager
+from myemail import *
 
 # Config
+SECRET_KEY = "development key"
 # Encryption config
 SECURITY_PASSWORD_SALT = 'securitykey'
+# Email config
+MAIL_SERVER = "smtp.gmail.com" # SMTP Server
+MAIL_PORT = 587 # SMTP Port
+MAIL_USE_TLS = True
+MAIL_USE_SSL = False
+
+MAIL_DEBUG = True
+# Login information
+MAIL_USERNAME = "noreply.wording@gmail.com"
+MAIL_PASSWORD = "TestingPassword"
+# Mail sender addres
+MAIL_DEFAULT_SENDER = "noreply.wording@gmail.com"
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -17,6 +32,9 @@ api = Api(app)
 
 # HTTP Authentication
 auth = HTTPBasicAuth()
+
+# Setup email
+mail = Mail(app)
 
 # REST Resources
 class User(Resource):
@@ -73,39 +91,41 @@ class List(Resource):
 # api.add_resource(User, '/<username>')
 api.add_resource(List, '/<username>/<listname>')
 
-
-
-# REST Recource with app.route
-@app.route('/<username>')
-@crossdomain(origin='*')
-@auth.login_required
-def get(username):
-
+# Register
+@app.route('/register', methods = ['POST'])
+def register():
 	db_manager = DatabaseManager()
+	
+	username = request.json.get('username')
+	password = sha512_crypt.encrypt(request.json.get('password'), salt=app.config['SECURITY_PASSWORD_SALT'], rounds=5000)
+	email = request.json.get('email')
+	if username is None or password is None or email is None:
+		abort(400) # missing arguments
+	if db_manager.username_exists(username) is not False or db_manager.email_exists(email) is not False:
+		abort(400)
 
-    if db_manager.username_exists(username):
-        user_info = db_manager.get_user(username)
-        list_lists = db_manager.get_lists_for_user(username)
-        for l in list_lists: del l['user_id']; del l['id']
-        
-        return json.dumps({
-            'username': user_info.get("username"),
-            'email' : user_info.get("email"),
-            'lists' : list_lists
-            })
-    else:
-        return json.dumps({
-            'username': 'ERROR: This shouldn\'t happen'
-            })
+	db_manager.create_user(username=username, password_hash=password, email=email, email_verified=False)
 
-# @app.route('/<username>/<list>')
-# def show_user_list(username, list):
-# 	return 'Show list ' + username + '/' + list
+	# Email verification
+	token = generate_confirmation_token(email)
+	confirm_url = url_for('verify_email', token=token, _external=True)
+	html = render_template('email.html', confirm_url=confirm_url)
+	subject = "Please confirm your email"
+	send_email(email, subject, html)
 
-# @app.route('/about')
-# def show_about():
-# 	return 'Insert about page here'
+	return "Successfully created user, please verify email.\n"
 
+# Verify email
+@app.route('/verify/<token>')
+def verify_email(token):
+	db_manager = DatabaseManager()
+	email = confirm_token(token)
+
+	if db_manager.email_verified(email):
+		return "Email already verified.\n"
+	else:
+		db_manager.verify_email(email)
+		return "Email Successfully verified.\n"
 
 # Verify password
 @auth.verify_password
@@ -122,6 +142,37 @@ def verify_password(username, password):
 	g.user = user
 	return True
 
+# REST Recource with app.route
+@app.route('/<username>')
+@crossdomain(origin='*')
+@auth.login_required
+def get(username):
+
+	db_manager = DatabaseManager()
+
+	if db_manager.username_exists(username):
+		user_info = db_manager.get_user(username)
+		list_lists = db_manager.get_lists_for_user(username)
+		for l in list_lists: del l['user_id']; del l['id']
+        
+		return json.dumps({
+			'username': user_info.get("username"),
+			'email' : user_info.get("email"),
+			'lists' : list_lists
+			})
+	else:
+		return json.dumps({
+			'username': 'ERROR: This shouldn\'t happen'
+			})
+
+# @app.route('/<username>/<list>')
+# def show_user_list(username, list):
+# 	return 'Show list ' + username + '/' + list
+
+# @app.route('/about')
+# def show_about():
+# 	return 'Insert about page here'
+
 # Login
 # @app.route('/login', methods = ['POST'])
 # def login():
@@ -135,24 +186,6 @@ def verify_password(username, password):
 
 #     if valid_login:
 #     	return "Successfully logged in"
-
-# Register
-@app.route('/register', methods = ['POST'])
-def register():
-	db_manager = DatabaseManager()
-	
-	username = request.json.get('username')
-	password = sha512_crypt.encrypt(request.json.get('password'), salt=app.config['SECURITY_PASSWORD_SALT'], rounds=5000)
-	email = request.json.get('email')
-	if username is None or password is None or email is None:
-		abort(400) # missing arguments
-	if db_manager.username_exists(username) is not False or db_manager.email_exists(email) is not False:
-		abort(400)
-
-	db_manager.create_user(username=username, password_hash=password, email=email, email_verified=False)
-
-    # TODO Create email verification
-
 
 
 # Run app
