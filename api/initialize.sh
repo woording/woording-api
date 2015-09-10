13d4b165753f91510c8ba2de430800de92fce33c
cor <cor@pruijs.nl>
2015-09-10 23:12:20 +0200

    Add initalize.sh and scss watch to gulp

--- api/initialize.sh
+++ api/initialize.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 
-./reset-database.sh
 ./create-env.sh
+. env/bin/activate
+./reset-database.sh
 python3 database_test.py
-
-clear
--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,11 @@ var sass = require('gulp-sass');
 var process = require('child_process');
 var shell = require('gulp-shell');
 
-gulp.task('default', ['start_api', 'start_web', 'scss_watch']);
+var runSequence = require('run-sequence');
 
+gulp.task('default', ['start']);
+
+gulp.task('initialize_api', shell.task(['cd api && ./initialize.sh']));
 gulp.task('start_api', shell.task(['cd api && . env/bin/activate && python api.py']));
 gulp.task('start_web', shell.task(['cd web && . ../api/env/bin/activate && python site.py']));
 
@@ -21,4 +24,26 @@ gulp.task('scss_watch', function() {
 	gulp.watch('web/static/scss/**/*.scss', ['scss']);
 });
 
+gulp.task('start', function(done) {
+	runSequence('initialize_api', function() {
+		gulp.start('start_api');
+		gulp.start('start_web');
+		gulp.start('scss_watch');
+
+	});
+});
+
+// // takes in a callback so the engine knows when it'll be done
+// gulp.task('one', function(cb) {
+//   // do stuff -- async or otherwise
+//   cb(err); // if err is not null and not undefined, the orchestration will stop, and 'two' will not run
+// });
+
+// // identifies a dependent task must be complete before this one begins
+// gulp.task('two', ['one'], function() {
+// 	gulp.start('start_api');
+// 	gulp.start('start_web');
+// 	gulp.start('scss_watch');
+//   // task 'one' is done now
+// });
 
