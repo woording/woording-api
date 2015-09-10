ff953dbbb82752f3394ae2340fa351490d2b6e57
cor <cor@pruijs.nl>
2015-09-10 23:17:18 +0200

    Cleanup gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var process = require('child_process');
 var shell = require('gulp-shell');
-
 var runSequence = require('run-sequence');
 
 gulp.task('default', ['start']);
-
 gulp.task('initialize_api', shell.task(['cd api && ./initialize.sh']));
+
 gulp.task('start_api', shell.task(['cd api && . env/bin/activate && python api.py']));
 gulp.task('start_web', shell.task(['cd web && . ../api/env/bin/activate && python site.py']));
 
@@ -29,21 +28,5 @@ gulp.task('start', function(done) {
 		gulp.start('start_api');
 		gulp.start('start_web');
 		gulp.start('scss_watch');
-
 	});
 });
-
-// // takes in a callback so the engine knows when it'll be done
-// gulp.task('one', function(cb) {
-//   // do stuff -- async or otherwise
-//   cb(err); // if err is not null and not undefined, the orchestration will stop, and 'two' will not run
-// });
-
-// // identifies a dependent task must be complete before this one begins
-// gulp.task('two', ['one'], function() {
-// 	gulp.start('start_api');
-// 	gulp.start('start_web');
-// 	gulp.start('scss_watch');
-//   // task 'one' is done now
-// });
-
