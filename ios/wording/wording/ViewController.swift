a02ffb5f7b412139ffdbd20932f8db829143a822
cor <cor@pruijs.nl>
2015-09-22 20:42:31 +0200

    Add boilerplate iOS code

---
+++ ios/wording/wording/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wording
+//
+//  Created by Cor Pruijs on 22-09-15.
+//  Copyright © 2015 Cor Pruijs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
