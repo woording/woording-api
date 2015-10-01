f7564f4f468a9b7e8b80bb0723b8f8d0824e5890
Leon <Leongrasmeijer@gmail.com>
2015-10-01 21:59:06 +0200

    Added prefixer to gulpfile, seems to work in safari now

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var sass = require('gulp-sass');
 var process = require('child_process');
 var shell = require('gulp-shell');
 var runSequence = require('run-sequence');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', ['start']);
 gulp.task('initialize_api', shell.task(['cd api && ./initialize.sh']));
@@ -16,6 +17,7 @@ gulp.task('start_web', shell.task(['cd web && . ../api/env/bin/activate && pytho
 gulp.task('scss', function() {
 	gulp.src('web/static/scss/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer())
 		.pipe(gulp.dest('web/static/css/'))
 });
 
