80d49a07da0e99f97566fd10152781cfc66c2d15
cor <cor@pruijs.nl>
2015-10-27 23:52:37 +0100

    Improve iOS app

--- ios/wording/wording/Translation.swift
+++ ios/wording/wording/Translation.swift
@@ -10,3 +10,10 @@ import Foundation
 
 typealias IsoCode = String
 typealias Translation = [IsoCode: String]
+
+func translationIsCorrect(translation: Translation) -> Bool {
+    
+    let correctTranslations: [Translation] = [["eng": "car", "dut": "auto"], ["eng": "tree", "dut": "boom"]]
+    
+    return correctTranslations.contains({$0 == translation})
+}
--- ios/wording/wording/ViewController.swift
+++ ios/wording/wording/ViewController.swift
@@ -13,12 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
         let translation1: Translation = ["eng": "car", "dut": "auto"]
+        let translation2: Translation = ["eng": "cer", "dut": "auto"]
         
         print(translation1["eng"])
         print(translation1["eng"]!)
         print(translation1["bulg"])
+        
+        print(translationIsCorrect(translation1))
+        print(translationIsCorrect(translation2))
         
     }
 
