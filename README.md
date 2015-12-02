afc8057c6582c35780a0a3a4e6829bb23fdd0a95
cor <cor@pruijs.nl>
2015-12-02 23:45:07 +0100

    Remove env/

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Wording
+woording-api
+============
 
 [![Join the chat at https://gitter.im/cor/Wording](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/cor/Wording?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
 
--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/cor/Development/woording/woording/env"
+set -gx VIRTUAL_ENV "/Users/cor/Development/woording/woording-api/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
